a155922d37fec0397104a560f9340de440303437
martin <martin.papanek@gmail.com>
2022-12-08 23:47:07 +0100

    tables

--- aoc.ex
+++ aoc.ex
@@ -14,6 +14,17 @@ defmodule Aoc do
     {index, element}
   end
 
+  def make_table(lists) do
+    list_of_maps = lists |> Enum.map(fn l -> l |> Enum.with_index(&Aoc.index_first/2) |> Enum.into(%{}) end)
+    Enum.with_index(list_of_maps, &Aoc.index_first/2) |> Enum.into(%{})
+  end
+
+  def table_entries(table) do
+    table
+    |> Map.to_list
+    |> Enum.flat_map(fn {k, v} -> Map.to_list(v) |> Enum.map(fn {k1, v1} -> {k, k1, v1} end) end)
+  end
+
   def transpose(lists) do
     lists |> Enum.zip |> Enum.map(&Tuple.to_list(&1))
   end
